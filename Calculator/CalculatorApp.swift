ff49dcdfbd701c13669a674926065f0fe54e303e
Samir-Sattarov <samirsattarov2004@gmail.com>
2022-09-08 15:37:48 +0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Samuel Sattarov on 08/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
